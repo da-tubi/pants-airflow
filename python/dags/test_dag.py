f75b0b17a0b2b03e16065bf40e57ae490d946d14
Da Shen <da@tubi.tv>
2023-04-04 16:26:23 +0800

    test dag

--- python/dags/test_dag.py
+++ python/dags/test_dag.py
@@ -2,15 +2,19 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 
+dag_args = {
+    'owner': 'da',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 3, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
 dag = DAG(
     dag_id='test_dag',
-    default_args={
-        'owner': 'da',
-        'depends_on_past': False,
-        'start_date': datetime(2023, 3, 1),
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-    },
+    default_args=dag_args,
+    schedule_interval='@daily',
 )
 
 task = BashOperator(
@@ -20,4 +24,3 @@ task = BashOperator(
 )
 
 task
-
