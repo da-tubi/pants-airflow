e2419bb02f04e28107ecaa5552697270c24a5e52
Da Shen <da@tubi.tv>
2023-09-13 16:52:34 +0800

    fix

--- airflow/airflow_config.py
+++ airflow/airflow_config.py
@@ -7,23 +7,29 @@ def config():
     print(f"dags_folder is {conf.get('core', 'dags_folder')}")
     pex_path = os.path.abspath(os.path.dirname(__file__))
     dags_folder = os.path.realpath(f"{pex_path}/dags")
-    if os.path.exists(AIRFLOW_CONFIG):
-        config = configparser.ConfigParser()
-        config.read(AIRFLOW_CONFIG)
+    print(f"airflow config {AIRFLOW_CONFIG}")
 
-        # rewrite the core dags_folder
-        if config.get("core", "dags_folder") != dags_folder:
-            config.set("core", "dags_folder", dags_folder)
-            with open(AIRFLOW_CONFIG, 'w') as f:
-                config.write(f)
-                print(f"overwrite dags_folder: {dags_folder}")
+    if not os.path.exists(AIRFLOW_CONFIG):
+        with open(AIRFLOW_CONFIG, 'w') as f:
+            conf.write(f)
 
-        # rewrite the core load_examples
-        if config.get("core", "load_examples") == 'True':
-            config.set("core", "load_examples", "False")
-            with open(AIRFLOW_CONFIG, 'w') as f:
-                config.write(f)
-                print(f"overwrite load_examples: False")
+    config = configparser.ConfigParser()
+    config.read(AIRFLOW_CONFIG)
+
+    # rewrite the core dags_folder
+    if config.get("core", "dags_folder") != dags_folder:
+        config.set("core", "dags_folder", dags_folder)
+        with open(AIRFLOW_CONFIG, 'w') as f:
+            config.write(f)
+            print(f"overwrite dags_folder: {dags_folder}")
+
+    # rewrite the core load_examples
+    if config.get("core", "load_examples") == 'True':
+        config.set("core", "load_examples", "False")
+        with open(AIRFLOW_CONFIG, 'w') as f:
+            config.write(f)
+            print(f"overwrite load_examples: False")
+
 
 if __name__ == "__main__":
     config()
